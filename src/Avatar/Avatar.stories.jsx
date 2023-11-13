55d9e43e64cbb090cd27c6a4b966bffbfc06c9fc
James Requenez <jrequenez@hotmail.com>
2023-11-13 16:17:30 -0600

    ✅ add stories to Avatar

--- src/Avatar/Avatar.stories.jsx
+++ src/Avatar/Avatar.stories.jsx
@@ -66,3 +66,14 @@ export const Large = {
     </>
   ),
 };
+
+export const Controls = {
+  args: {
+    loading: false,
+    size: 'tiny',
+    username: 'Dominic Nguyen',
+    src: 'https://avatars.githubusercontent.com/u/263385',
+  }
+}
+
+export const NoArgs = {};
